6eb400d6976df9c020dfde9ff2e35b0cc47b3585
Joe Burns <josebur86@gmail.com>
2016-10-22 15:08:23 -0500

    Initial implementation of adding a person the db.

--- predictor/main.py
+++ predictor/main.py
@@ -1,6 +1,10 @@
-from utility import Ameren
+from utility import Ameren, Laclede
 from flask import Flask
 from flask import request
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from data.db import db, Person
 
 app = Flask(__name__)
 
@@ -12,6 +16,7 @@ def hello():
 def get_at_risk_customers():
     utilities_to_check = []
     utilities_to_check.append(Ameren())
+    utilities_to_check.append(Laclede())
 
     at_risk_customers = set()
 
@@ -21,7 +26,12 @@ def get_at_risk_customers():
         # For now, all unpaid customers will be considered at-risk customers.
         at_risk_customers = at_risk_customers.union(unpaid_customers)
 
-    #TODO: Insert the at-risk customers into the database.
+    # Insert the at-risk customers into the database.
+    for customer in at_risk_customers:
+        person = Person(customer.first_name, customer.last_name, customer.phone_number, 1)
+        db.session.add(person)
+        db.session.commit()
+
     #TODO: Based on the at-risk customers we have found, let's compute the risk_index of each customer.
 
     # Return all of the customers as a string for now.
