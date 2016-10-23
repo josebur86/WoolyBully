b093995cbad7695932cb5e94a30ff1a2208d3e1d
Olivia G <ogonzalez730@gmail.com>
2016-10-23 03:04:38 -0500

    added contact dates

--- ui/routes.py
+++ ui/routes.py
@@ -5,13 +5,14 @@ from data.db import DataHelper, db
 from ui import app
 from forms import signInForm
 
+last_contacted = ["March 18, 2016", "October 21, 2016", "January 1, 2016","September 7, 2016","October 2, 2016","July 1, 2016","October 9, 2016"]
+
 @app.route('/')
 def Index():
     db.create_all()
     db_helper = DataHelper()
     persons = db_helper.get_all_people();
-
-    return render_template('index.html', persons=persons, db_helper=db_helper)
+    return render_template('index.html', persons=persons, db_helper=db_helper, contact_dates=last_contacted)
 
 @app.route('/login')
 def log_in():
