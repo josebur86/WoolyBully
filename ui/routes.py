fabe5380dc2fea888dd52255fbc970b72ec830e0
Olivia G <ogonzalez730@gmail.com>
2016-10-23 03:27:04 -0500

    Added tooltip to shutoff date image

--- ui/routes.py
+++ ui/routes.py
@@ -6,13 +6,14 @@ from ui import app
 from forms import signInForm
 
 last_contacted = ["March 18, 2016", "October 21, 2016", "January 1, 2016","September 7, 2016","October 2, 2016","July 1, 2016","October 9, 2016"]
+shutoff = last_contacted = ["October 31, 2016", "October 31, 2016","November 7, 2016","November 2, 2016","November 1, 2016","October 29, 2016"]
 
 @app.route('/')
 def Index():
     db.create_all()
     db_helper = DataHelper()
     persons = db_helper.get_all_people();
-    return render_template('index.html', persons=persons, db_helper=db_helper, contact_dates=last_contacted)
+    return render_template('index.html', persons=persons, db_helper=db_helper, contact_dates=last_contacted, shutoff_dates=shutoff)
 
 @app.route('/login')
 def log_in():
